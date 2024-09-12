fd696c5dac59775102ee7434febfdac255dd74f9
GayathriFS <gayathrisit94@gmail.com>
2024-09-12 18:11:05 +0530

    update project

---
+++ src/components/app.js
@@ -0,0 +1,18 @@
+import Navbar from './navbar';
+import About from './about';
+import Search from './search';
+import Product from './product';
+import Footer from './footer';
+function App() {
+return(
+    <>
+         <Navbar></Navbar>
+    <Search></Search>
+    <Product></Product>
+    <About></About>
+    <Footer></Footer>
+    </>
+)
+}
+ 
+export default App
---
+++ src/components/search.js
@@ -0,0 +1,12 @@
+//Search Components
+
+function Search()
+{
+  return(
+    <div className='searchbar'>
+      <input type='text' placeholder='Search for Products'></input>
+    </div>
+  )
+}
+
+export default Search
